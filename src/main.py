3b8c45ab3b1e096aa436e92cb6ad3642b7c1eca2
FabBeyond <redcraft42.12@gmail.com>
2026-09-22 16:06:26 +0200

    Lowkey got no idea what im doing

--- src/main.py
+++ src/main.py
@@ -1,17 +1,11 @@
-from rich.live import Live
-from rich.text import Text
+from rich.console import Console
 import time
 import requests
 
-def render_lyrics(lines, cur_idx):
-    text = Text()
-    for i, line in enumerate(lines):
-        style = "bold cyan" if i == cur_idx else "dim"
-        text.append(line + "\n", style=style)
-    return text
+console = Console()
 
 try:
-    response = requests.get("https://lrclib.net/api/search", params={"q": "Still Shining mayonazy"}, headers={"User-Agent": "terminal-lyrics/0.0.1"})
+    response = requests.get("https://lrclib.net/api/search", params={"q": "tetoris"}, headers={"User-Agent": "terminal-lyrics/0.0.1"})
     if response.status_code == 200:
         response.encoding = "utf-8"
         data = response.json()
@@ -20,10 +14,8 @@ try:
             pass
         else:
             lyrics = lyrics.split("\n")
-            with Live(render_lyrics(lyrics, 0), refresh_per_second=4) as live:
-                for i in range(len(lyrics)):
-                    live.update(render_lyrics(lyrics, i))
-                    time.sleep(0.5)
+            for line in lyrics:
+                console.print(line, justify="center", align="center")
 
 except requests.exceptions.RequestException as errex:
     print("Exception request")
