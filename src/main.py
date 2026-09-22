f2bac2e88c8384162738339b028ac34c8cfe9588
FabBeyond <redcraft42.12@gmail.com>
2026-09-22 16:45:12 +0200

    Added time synchro
    Lowkey didnt test it but fuck it we ball

--- src/main.py
+++ src/main.py
@@ -2,8 +2,18 @@ from rich.console import Console
 from rich.align import Align
 import time
 import requests
+from datetime import datetime
 
 console = Console()
+
+def get_time_until_next(line1, line2):
+    cur_lyric_time = line1.split("]")[0].replace("[", "")
+    next_lyric_time = line2.split("]")[0].replace("[", "")
+
+    date1 = datetime.strptime(cur_lyric_time, "%M:%S.%f")
+    date2 = datetime.strptime(next_lyric_time, "%M:%S.%f")
+
+    return (date2-date1).total_seconds()
 
 try:
     response = requests.get("https://lrclib.net/api/search", params={"q": "tetoris"}, headers={"User-Agent": "terminal-lyrics/0.0.1"})
@@ -15,14 +25,17 @@ try:
             pass
         else:
             lyrics = lyrics.split("\n")
-            for line in lyrics:
+            for idx, line in enumerate(lyrics):
+                text = line.split("]")[1]
+
                 text = Align(
-                    line,
+                    text,
                     vertical="middle",
                     align="center",
                     height=console.height
                 )
                 console.print(text)
+                time.sleep(get_time_until_next(line, lyrics[idx+1]))
 
 except requests.exceptions.RequestException as errex:
     print("Exception request")
