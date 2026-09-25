cc78bd493e6e1190f3db9ee535bf399f4d1f542e
FabBeyond <redcraft42.12@gmail.com>
2026-09-25 21:38:47 +0200

    Fix keybind cleanup and add settings for font size

--- src/litt/main.py
+++ src/litt/main.py
@@ -25,11 +25,14 @@ if subprocess.run(["which", "playerctl"], capture_output=True, text=True).return
     print("Playerctl isnt installed or added to PATH")
     sys.exit(1)
 
+old_settings = termios.tcgetattr(sys.stdin.fileno())
+tty.setcbreak(sys.stdin.fileno())
+
 settings = {}
 config_dir = os.environ.get("XDG_CONFIG_HOME", os.path.expanduser("~/.config"))
 config_path = os.path.join(config_dir, "litt")
 config_file = os.path.join(config_path, "config.json")
-DEFAULTS = {"font": "base", "font_style": "blocky"}
+DEFAULTS = {"font": "base", "font_style": "blocky", "font_size": 25}
 os.makedirs(config_path, exist_ok=True)
 if not os.path.isfile(config_file):
     with open(config_file, "w") as f:
@@ -62,8 +65,6 @@ selected_player = player_menu.show()
 if selected_player is None:
     sys.exit(0)
 selected_player = players[selected_player]
-old_settings = termios.tcgetattr(sys.stdin.fileno())
-tty.setcbreak(sys.stdin.fileno())
 
 POLL_RATE = 0.1
 
@@ -91,7 +92,8 @@ def wait_until_next_song():
     while get_playing_song() == last_song:
         last_song = get_playing_song()
         time.sleep(1)
-def blockify(text, consoleWidth, font_path="", size=25):
+def blockify(text, consoleWidth, font_path=""):
+    size = settings["font_size"]
     if settings["font"] == "base":
         font_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "fonts", "NotoSansCJK-Regular.ttc")
     else:
@@ -185,97 +187,100 @@ def wrap_text(text, font, max_width):
     return "\n".join(lines)
 
 def main():
-    while True:
-        console.clear()
-        try:
-            playing_song, artist = get_playing_song()
-            lyrics = syncedlyrics.search(f"{playing_song} {artist}")
-        except RequestException as e:
-            print(f"Network error {e}")
-            wait_until_next_song()
-            continue
-        except Exception as e:
-            print(f"An unexpected error occured {e}")
-            wait_until_next_song()
-            continue
-
-        if lyrics is None:
-            print("no lyrics :(")
-            wait_until_next_song()
-            continue
-        if lyrics == []:
-            print("no lyrics :(")
-            wait_until_next_song()
-            continue
-
-        lyrics = lyrics.split("\n")
-        idx = 0
-        last_song = get_playing_song()
-        global_offset = list(filter(lambda x: "offset" in x.lower(), lyrics))
-        if len(global_offset) == 0:
-            global_offset = 0
-        else:
-            global_offset = list(global_offset)[0]
-            global_offset = float(global_offset.split(":")[1].strip()[:-1])
-
-        timed_lyrics = []
-        for lyric in lyrics:
-            timestamp = parse_timestamp(lyric)
-            if timestamp is None:
-                continue
-            timestamp += global_offset
-
-            timed_lyrics.append((timestamp, lyric.split("]")[1].strip()))
-
-        last_song = get_playing_song()
-
+    try:
         while True:
-            pos = get_position()
-            text = list(filter(lambda x: x[0] <= pos+global_offset, timed_lyrics))
-            if len(text) == 0:
+            console.clear()
+            try:
+                playing_song, artist = get_playing_song()
+                lyrics = syncedlyrics.search(f"{playing_song} {artist}")
+            except RequestException as e:
+                print(f"Network error {e}")
+                wait_until_next_song()
                 continue
-            content = text[len(text)-1][1].strip()
+            except Exception as e:
+                print(f"An unexpected error occured {e}")
+                wait_until_next_song()
+                continue
 
-            new_song = get_playing_song()
-            if new_song != last_song:
-                break
-            last_song = new_song
+            if lyrics is None:
+                print("no lyrics :(")
+                wait_until_next_song()
+                continue
+            if lyrics == []:
+                print("no lyrics :(")
+                wait_until_next_song()
+                continue
 
-            banner = Text(content, justify="center")
+            lyrics = lyrics.split("\n")
+            idx = 0
+            last_song = get_playing_song()
+            global_offset = list(filter(lambda x: "offset" in x.lower(), lyrics))
+            if len(global_offset) == 0:
+                global_offset = 0
+            else:
+                global_offset = list(global_offset)[0]
+                global_offset = float(global_offset.split(":")[1].strip()[:-1])
 
-            text = Align(
-                blockify(content, console.width),
-                vertical="middle",
-                align="center",
-                height=console.height
-            )
-            console.print(text)
+            timed_lyrics = []
+            for lyric in lyrics:
+                timestamp = parse_timestamp(lyric)
+                if timestamp is None:
+                    continue
+                timestamp += global_offset
 
-            ready, _, _ = select.select([sys.stdin], [], [], POLL_RATE)
-            if ready:
-                key = sys.stdin.read(1)
-                if key == "q":
-                    sys.exit(0)
-                elif key == ",":
-                    # change font size
-                    pass
-                elif key == ".":
-                    # change font size
-                    pass
-                elif key == "r":
+                timed_lyrics.append((timestamp, lyric.split("]")[1].strip()))
+
+            last_song = get_playing_song()
+
+            while True:
+                pos = get_position()
+                text = list(filter(lambda x: x[0] <= pos+global_offset, timed_lyrics))
+                if len(text) == 0:
+                    continue
+                content = text[len(text)-1][1].strip()
+
+                new_song = get_playing_song()
+                if new_song != last_song:
                     break
-                elif key == "j":
-                    # adjust song offset
-                    pass
-                elif key == "k":
-                    # adjust song offset
-                    pass
-                elif key == "J":
-                    # adjust global offset
-                    pass
-                elif key == "K":
-                    # adjust global offset
-                    pass
+                last_song = new_song
+
+                banner = Text(content, justify="center")
+
+                text = Align(
+                    blockify(content, console.width),
+                    vertical="middle",
+                    align="center",
+                    height=console.height
+                )
+                console.print(text)
+
+                ready, _, _ = select.select([sys.stdin], [], [], POLL_RATE)
+                if ready:
+                    key = sys.stdin.read(1)
+                    if key == "q":
+                        sys.exit(0)
+                    elif key == ",":
+                        # change font size
+                        pass
+                    elif key == ".":
+                        # change font size
+                        pass
+                    elif key == "r":
+                        break
+                    elif key == "j":
+                        # adjust song offset
+                        pass
+                    elif key == "k":
+                        # adjust song offset
+                        pass
+                    elif key == "J":
+                        # adjust global offset
+                        pass
+                    elif key == "K":
+                        # adjust global offset
+                        pass
+    finally:
+        termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, old_settings)
 
 if __name__ == "__main__":
     main()
--- src/litt/settings.py
+++ src/litt/settings.py
@@ -6,7 +6,7 @@ config_dir = os.environ.get("XDG_CONFIG_HOME", os.path.expanduser("~/.config"))
 config_file = os.path.join(config_dir, "litt", "config.json")
 settings = json.loads(open(config_file, "r").read())
 
-font_options = ["Style", "Font"]
+font_options = ["Style", "Font", "Font Size"]
 font_menu = TerminalMenu(font_options, title="Settings")
 answer = font_menu.show()
 
@@ -29,5 +29,8 @@ elif answer == 1:
         settings["font"] = "base"
     elif answer == 1:
         settings["font"] = input("Custom Font Path: ")
+
+elif answer == 2:
+    settings["font_size"] = float(input(f"Set custom font size (cur: {settings['font_size']}): "))
 
 open(config_file, "w").write(json.dumps(settings, indent=2))
